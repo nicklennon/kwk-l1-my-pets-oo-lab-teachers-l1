c23a29e03d2432c97915578d8236364cc6216f22
nicklennon <nicklennon.53@gmail.com>
2018-06-24 17:58:03 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,4 +1,31 @@
 class Owner
   # code goes here
-
+  attr_reader :name, :species
+  attr_accessor :mood, :name, :pets
+  
+  def initialize(species)
+    @species = species
+    @pets={:fishes => [], :dogs => [], :cats => []}
+  end
+  
+  def say_species
+    "I am a #{@species}."
+  end
+  
+  def buy_fish(name)
+    @pets[:fishes] << Fish.new(name).name
+  end
+  
+  def buy_cat(name)
+    @pets[:cats] << Cat.new(name).name
+  end
+  
+  def buy_dog(name)
+    @pets[:dogs] << Dog.new(name).name
+  end
+  
+  def list_pets
+    "I have #{@pets[:fishes].length} fish, #{pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
+  end
+  
  end
